c56c48196ba12ec86d94e54560f778b3c2ccf1c5
Danilov Pjotr <danilovpjotr@gmail.com>
2023-04-27 14:11:45 +0300

    Добавьте файлы проекта.

---
+++ LibraryApiTests/Book.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace LibraryApiTests
+{
+    public class Book
+    {
+        public string author { get; set; }
+        public int id { get; set; }
+        public bool isElectronicBook { get; set; }
+        public string name { get; set; }
+        public int year { get; set; }
+    }
+    public class BookFromResponseBody
+    {
+        public Book book { get; set; }
+    }
+    public class BooksFromResponseBody
+    {
+        public List<Book> books { get; set; }
+    }
+    public class ResultFromResponseBody
+    {
+        public bool result { get; set; }
+    }
+}
